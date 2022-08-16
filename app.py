fee3ba9340c173eeb49e99a3d3485264c9a76ee5
kudchemanasi <manasikudche123@gmail.com>
2022-08-16 15:03:20 +0530

    Housing

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+from housing.logger import logging
+
 
 @app.route("/", methods=['GET','POST'])
 def index():
+    logging.info("We are testing logging module")
     return "Starting Machine Learning Project "
 
 
